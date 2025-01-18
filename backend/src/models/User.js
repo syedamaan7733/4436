d7dec2e46b65501829dcbf3572bbc16818498273
syedamaan7733 <codegeek7733@gmail.com>
2025-01-18 15:01:25 +0530

    removed .env file

--- backend/src/models/User.js
+++ backend/src/models/User.js
@@ -30,6 +30,10 @@ const userSchema = new mongoose.Schema(
       type: String,
       default: "",
     },
+    bio: {
+      type: String,
+      default: "",
+    },
   },
   {
     timestamps: true,
