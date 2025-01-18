a7bf4ccd84d047ad23ad8e9610157dfaa0e1c811
syedamaan7733 <codegeek7733@gmail.com>
2025-01-18 19:17:54 +0530

    frontend proj

---
+++ backend/src/middleware/cloudinary.service.js
@@ -0,0 +1,36 @@
+// Required dependencies
+const express = require("express");
+const router = express.Router();
+const multer = require("multer");
+const { v2: cloudinary } = require("cloudinary");
+const auth = require("../middleware/auth");
+const User = require("../models/User");
+const { useLoaderData } = require("react-router-dom");
+// Cloudinary configuration
+cloudinary.config({
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
+});
+
+// Utility function for uploading to Cloudinary
+const uploadToCloudinary = async (fileBuffer, folder) => {
+  try {
+    const result = await cloudinary.uploader.upload_stream(
+      {
+        folder,
+      },
+      fileBuffer
+    );
+    return result.secure_url;
+  } catch (error) {
+    console.error("Error uploading to Cloudinary:", error);
+    throw new Error("Failed to upload image");
+  }
+};
+
+// Multer setup for storing image in memory
+const upload = multer({ storage: multer.memoryStorage() });
+
+
+module.exports = {uploadToCloudinary, upload}
