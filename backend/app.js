e5729e1c67fececae35bba8fabd45bb19a259ef4
Syed Amaan Ali <amaan7733@Syeds-MacBook-Air.local>
2025-06-18 20:49:02 +0530

    aah kil me

--- backend/app.js
+++ backend/app.js
@@ -17,7 +17,7 @@ connectDB();
 app.use(
   cors({
     origin: [
-      "https://4436-i4eo.vercel.app",
+      "https://4436-euiu.vercel.app",
       "http://127.0.0.1:5500",
       "http://127.0.0.1:5501",
       "http://127.0.0.1:5502",
